d88cb4cc08b6c4b1662ff495dc4e246e7c86bfca
tospery <tospery@gmail.com>
2024-03-17 18:40:34 +0800

    grpc

---
+++ hello_grpc_case/server/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+	pb "mygo/hello_grpc_case/proto" // 引入编译生成的包
+)
+
+const (
+	// Address gRPC服务地址
+	Address = "127.0.0.1:50052"
+)
+
+// 定义helloService并实现约定的接口
+type helloService struct{}
+
+// SayHello 实现Hello服务接口
+func (h helloService) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	resp := new(pb.HelloResponse)
+	resp.Message = fmt.Sprintf("Hello %s.", in.Name)
+
+	return resp, nil
+}
+
+// HelloService Hello服务
+var HelloService = helloService{}
+
+func main() {
+	listen, err := net.Listen("tcp", Address)
+	if err != nil {
+		grpclog.Fatalf("Failed to listen: %v", err)
+	}
+
+	// 实例化grpc Server
+	s := grpc.NewServer()
+
+	// 注册HelloService
+	pb.RegisterHelloServer(s, HelloService)
+
+	grpclog.Println("Listen on " + Address)
+	fmt.Println("Listen on ", Address)
+	s.Serve(listen)
+}
